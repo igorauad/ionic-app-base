3dae409ce70b02e73300da79be772531298c5c63
Igor Freire <igor.auad@gmail.com>
2014-10-25 20:18:07 -0300

    Add application configuration module
    Angular initialization into config.js and application.js. Add basic `modules` scaffold.

---
+++ www/application.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//Start by defining the main module and adding the module dependencies
+angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
+
+
+angular.module(ApplicationConfiguration.applicationModuleName).run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // Set the statusbar to use the default style, tweak this to
+      // remove the status bar on iOS or change it to use white instead of dark colors.
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+//Then define the init function for starting up the application
+angular.element(document).ready(function() {
+	//Then init the app
+	angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
+});
+
